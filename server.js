fd0ab9d8dc6a169422ace147f30f6d56fd98294d
ogata216 <amidstasinglebreath@gmail.com>
2019-12-13 00:08:21 -0800

    Attempt to resolve file conflict & replication error during overhaul

--- server.js
+++ server.js
@@ -59,64 +59,6 @@ function start() {
         });
 }
 
-let companyData = new Object();
-connection.connect(function (err) {
-    if (err) throw err;
-    let companyStructQuery = "select role.id, title, salary,department_id, name AS department from role join department on role.department_id = department.id;";
-    connection.query(companyStructQuery, function (err, res) {
-        if (err) throw err;
-        for (var i = 0; i < res.length; i++) {
-            companyData[res[i].title] = res[i];
-        }
-        start();
-    });
-});
-
-function start() {
-    // console.log(companyData);
-    inquirer.prompt({
-        type: "list",
-        message: "What do you want to do?",
-        name: "action",
-        choices: [
-            "View all the employees",
-            "View all the employees by department",
-            "View all the employees by manager",
-            "Add Employee",
-            "Remove Employee",
-            "Update Employee Role",
-            "Update Employee Department"
-        ]
-    })
-        .then(function (userAns) {
-            let userAction = userAns.action;
-            switch (userAction) {
-                case ("View all the employees"):
-                    viewEmployee();
-                    break;
-                case ("View all the employees by department"):
-                    viewEmployeeByDept();
-                    break;
-                case ("View all the employees by manager"):
-                    viewEmployeeByManager();
-                    break;
-                case ("Add Employee"):
-                    addEmployee();
-                    break;
-                case ("Remove Employee"):
-                    removeEmployee();
-                    break;
-                case ("Update Employee Role"):
-                    updateRole();
-                    break;
-                case ("Update Employee Manager"):
-                    updateManager();
-                    break;
-            }
-        });
-}
-
-
 function viewEmployee() {
     console.log("\n\n");
     let query = `Select e.id,e.first_name, e.last_name, title, name 'department',salary, IFNULL(CONCAT(m.first_name, ' ' , m.last_name),
